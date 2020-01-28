1cde8f80d4d17bbc29b70227567b8a544488c491
Arjun Roy <arjunroychoudhry4059@gmail.com>
2020-01-28 21:05:54 +0000

    Step 17

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,21 +1,34 @@
 class DockingStation
-
-  attr_reader :bike
+  DEFAULT_CAPACITY = 20
+  attr_accessor :capacity
+  def initialize(capacity=DEFAULT_CAPACITY)
+    @bike = []
+    @capacity = capacity
+  end
 
   def release_bike
-    if @bike then
-      @bike
-    else
-      raise 'there are no bikes'
-    end
+    fail 'there are no bikes' if empty?
+    @bike.pop
   end
 
   def dock(bike)
-    @bike = bike
+    if full?
+      then
+      raise 'there is already a bike'
+    end
+    @bike.push(bike)
+    bike
   end
-  #
-  # def bike
-  #   @bike
-  # end
 
+  private
+
+  attr_reader :bike
+
+  def full?
+    @bike.count >= DEFAULT_CAPACITY
+  end
+
+  def empty?
+    @bike.empty?
+  end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -1,7 +1,16 @@
 require 'DockingStation'
 
-
 describe DockingStation do
+  describe 'initialization' do
+    subject { DockingStation.new }
+    let(:bike) { Bike.new }
+    it 'has a default capacity' do
+      DockingStation::DEFAULT_CAPACITY.times do
+      subject.dock(bike)
+    end
+    expect{ subject.dock Bike.new }.to raise_error 'there is already a bike'
+    end
+  end
   it 'responds to release_bike' do
     expect(DockingStation.new).to respond_to (:release_bike)
   end
@@ -16,20 +25,25 @@ describe DockingStation do
     expect(DockingStation.new).to respond_to(:dock).with(1).argument
   end
   it 'docks a bike' do
-    bike = Bike.new
-    expect(DockingStation.new.dock(bike)).to eq bike
-  end
-  it 'responds to bike' do
-    expect(DockingStation.new).to respond_to(:bike)
-  end
-  it 'returns a Docked Bike' do
-    bike = Bike.new
-    dock = DockingStation.new
-    dock.dock(bike)
-    expect(dock.bike).to eq bike
+    docking_station = DockingStation.new
+    DockingStation::DEFAULT_CAPACITY.times do
+      bike = Bike.new
+      expect(docking_station.dock(bike)).to eq bike
+    end
   end
   it 'returns an error when there is no bike' do
     dock = DockingStation.new
-    expect{dock.release_bike}.to raise_error
+    expect{dock.release_bike}.to raise_error 'there are no bikes'
+  end
+  it 'raises an error when there is already a bike' do
+    docking_station = DockingStation.new
+    DockingStation::DEFAULT_CAPACITY.times do
+      bike = Bike.new
+      expect(docking_station.dock(bike)).to eq bike
+    end
+    expect{docking_station.dock(Bike.new)}.to raise_error 'there is already a bike'
+  end
+  it 'has a default capacity' do
+    expect(DockingStation.new.capacity).to eq DockingStation::DEFAULT_CAPACITY
   end
 end
